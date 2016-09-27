e0f759523386dfbfb09037715f84731501b40ad3
Eric-Arellano <ericarellano@me.com>
2016-09-26 19:20:23 -0500

    add convertKey method

--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -4,6 +4,15 @@ public class VigenereCipher implements Encryptable {
 		return "";
 	}
 
-
+	private int[] convertKey(String key) {
+		char[] keys = key.toCharArray();
+		int[] shifts = new int[keys.length];
+		for (int index = 0; index < keys.length; index++) {
+			shifts[index] = keys[index];
+			shifts[index] = (shifts[index] - 65) % 26;
+			shifts[index]++;
+		}
+		return shifts;
+	}
 
 }
