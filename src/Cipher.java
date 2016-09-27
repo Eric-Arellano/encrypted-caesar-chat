62a29ab8018bfaaa7befc0165053ed65e0120e98
Eric-Arellano <ericarellano@me.com>
2016-09-26 20:05:49 -0500

    add support for extended key phrase

--- src/Cipher.java
+++ src/Cipher.java
@@ -5,8 +5,8 @@ public class Cipher {
 
 	public static void main(String[] args) {
 
-		String sampleMessage = "POTUSINTHEHOSUE";
-		String sampleKey = "A";
+		String sampleMessage = "TESTTEST";
+		String sampleKey = "HOLA";
 		VigenereCipher sample = new VigenereCipher();
 
 		System.out.print(sample.encryptMessage(sampleMessage, sampleKey));
--- src/VigenereCipher.java
+++ src/VigenereCipher.java
@@ -8,11 +8,22 @@ public class VigenereCipher implements Encryptable {
 
 	public String encryptMessage(String message, String key) {
 		StringBuilder messageBuilder = new StringBuilder();
+		int keyIndex = 0;
 		for (int messageIndex = 0; messageIndex < message.length(); messageIndex++ ) {
+			// get current values
 			String substring = message.substring(messageIndex, messageIndex + 1);
+			String currentKey = key.substring(keyIndex, keyIndex + 1);
 
-			String encryption = caesarCipher.encryptMessage(substring, key);
+			// append encryption
+			String encryption = caesarCipher.encryptMessage(substring, currentKey);
 			messageBuilder.append(encryption);
+
+			// update keyIndex
+			if (keyIndex < key.length() - 1) {
+				keyIndex++;
+			} else {
+				keyIndex = 0;
+			}
 		}
 		return messageBuilder.toString();
 	}
