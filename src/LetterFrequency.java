957880ab6cd6751d331029e5068d6fa195cb3232
Eric-Arellano <ericarellano@me.com>
2016-09-26 21:59:41 -0500

    LetterFrequency framework

---
+++ src/LetterFrequency.java
@@ -0,0 +1,51 @@
+import java.util.LinkedHashMap;
+
+public class LetterFrequency {
+
+	private LinkedHashMap<Character, Integer> letterMap;
+
+	public LetterFrequency() {
+		letterMap = new LinkedHashMap<>();
+
+		letterMap.put('A', 0);
+		letterMap.put('B', 0);
+		letterMap.put('C', 0);
+		letterMap.put('D', 0);
+		letterMap.put('E', 0);
+		letterMap.put('F', 0);
+		letterMap.put('G', 0);
+		letterMap.put('H', 0);
+		letterMap.put('I', 0);
+		letterMap.put('J', 0);
+		letterMap.put('K', 0);
+		letterMap.put('L', 0);
+		letterMap.put('M', 0);
+		letterMap.put('N', 0);
+		letterMap.put('O', 0);
+		letterMap.put('P', 0);
+		letterMap.put('Q', 0);
+		letterMap.put('R', 0);
+		letterMap.put('S', 0);
+		letterMap.put('T', 0);
+		letterMap.put('U', 0);
+		letterMap.put('V', 0);
+		letterMap.put('X', 0);
+		letterMap.put('Y', 0);
+		letterMap.put('Z', 0);
+	}
+
+	public void analyzeLetterFrequency(String message) {
+		// count letter frequency
+
+		// display letter frequency
+	}
+
+	private void countLetterFrequency(String message) {
+		// upon reading key value, increment count value
+	}
+
+	private void displayLetterFrequency() {
+		// print visualization and/or table
+	}
+
+}
