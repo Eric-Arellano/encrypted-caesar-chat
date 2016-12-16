9b350f79ab78168f504617367e78cad7c86555fb
Eric Arellano <ericarellano@me.com>
2016-12-16 15:33:11 -0700

    fix formatting

--- src/encryptors/VigenereCipher.java
+++ src/encryptors/VigenereCipher.java
@@ -31,33 +31,33 @@ public class VigenereCipher implements Encryptable, Decryptable {
 		int keyIndex = 0;
 		for (int messageIndex = 0; messageIndex < encryptedMessage.length(); messageIndex++ ) {
 			// get current values
-            String currentLetter = getCurrentLetter(encryptedMessage, messageIndex);
-            String currentKey = getCurrentKey(key, keyIndex);
+			String currentLetter = getCurrentLetter(encryptedMessage, messageIndex);
+			String currentKey = getCurrentKey(key, keyIndex);
 
 			// append encryption
-            String encryptedLetter = caesarCipher.decryptMessage(currentLetter, currentKey);
-            messageBuilder.append(encryptedLetter);
+			String encryptedLetter = caesarCipher.decryptMessage(currentLetter, currentKey);
+			messageBuilder.append(encryptedLetter);
 
 			// update keyIndex
-            keyIndex = updateKeyIndex(keyIndex, key.length());
+			keyIndex = updateKeyIndex(keyIndex, key.length());
 		}
 		return messageBuilder.toString();
 	}
 
     private String getCurrentLetter(String message, int messageIndex) {
-        return message.substring(messageIndex, messageIndex + 1);
+		return message.substring(messageIndex, messageIndex + 1);
     }
 
     private String getCurrentKey(String key, int keyIndex) {
-        return key.substring(keyIndex, keyIndex + 1);
+		return key.substring(keyIndex, keyIndex + 1);
     }
 
     private int updateKeyIndex(int keyIndex, int keyLength) {
-        if (keyIndex < keyLength - 1) {
-            return keyIndex + 1;
-        } else {
-            return 0;
-        }
-    }
+		if (keyIndex < keyLength - 1) {
+			return keyIndex + 1;
+		} else {
+			return 0;
+		}
+	}
 
 }
