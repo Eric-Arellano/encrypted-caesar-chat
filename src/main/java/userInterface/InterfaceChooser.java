9c52bcea3a3ae51da51cf744afcc74da59ababd5
Eric-Arellano <ericarellano@me.com>
2016-12-18 23:22:58 -0700

    add chatapp to interface chooser and fix command line logic

--- src/main/java/userInterface/InterfaceChooser.java
+++ src/main/java/userInterface/InterfaceChooser.java
@@ -1,5 +1,6 @@
 package userInterface;
 
+import userInterface.networking.ChatApp;
 import userInterface.networking.NetworkingApp;
 import userInterface.utilities.InvalidInputException;
 
@@ -20,6 +21,8 @@ public class InterfaceChooser implements Launchable {
 				launchCommandLineApp();
 			} else if (isNetworkingApp()) {
 				launchNetworkingApp();
+			} else if (isChatApp()) {
+				launchChatApp();
 			} else {
 				throw new InvalidInputException("Invalid command line arguments.");
 			}
@@ -27,20 +30,56 @@ public class InterfaceChooser implements Launchable {
 			System.out.println("Invalid command line arguments.");
 			System.exit(1);
 		}
-
 	}
+
+
+	// -----------------------------------------------------------------------------------
+	// Determine app type
+	// -----------------------------------------------------------------------------------
 
 	private boolean isConsoleApp() {
 		return args.length == 0;
 	}
 
 	private boolean isCommandLineApp() {
+		return isCommandLineArgsLength() && isNotChatApp();
+	}
+
+	private boolean isCommandLineArgsLength() {
 		return args.length == 3;
 	}
 
 	private boolean isNetworkingApp() {
+		return isNetworkingArgsLength() && isNotChatApp();
+	}
+
+	private boolean isNetworkingArgsLength() {
 		return args.length == 1 || args.length == 2 || args.length == 4 || args.length == 5;
 	}
+
+	private boolean isNotChatApp() {
+		return !isChatApp();
+	}
+
+	private boolean isChatApp() {
+		return isChatArgsLength() && isChatPrefix();
+	}
+
+	private boolean isChatArgsLength() {
+		return args.length == 2 || args.length == 3;
+	}
+
+	private boolean isChatPrefix() {
+		String prefix = args[0];
+		return prefix.equalsIgnoreCase("chat")
+				|| prefix.equalsIgnoreCase("c")
+				|| prefix.equalsIgnoreCase("-c");
+	}
+
+
+	// -----------------------------------------------------------------------------------
+	// Launch apps
+	// -----------------------------------------------------------------------------------
 
 	private void launchConsoleApp() {
 		app = new ConsoleApp();
@@ -54,6 +93,11 @@ public class InterfaceChooser implements Launchable {
 
 	private void launchNetworkingApp() {
 		app = new NetworkingApp(args);
+		app.launchApp();
+	}
+
+	private void launchChatApp() {
+		app = new ChatApp(args);
 		app.launchApp();
 	}
 
