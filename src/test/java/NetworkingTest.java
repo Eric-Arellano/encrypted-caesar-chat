c1cc41ca96869b452d8cd034923da1225d1ce344
Eric-Arellano <ericarellano@me.com>
2017-05-15 22:12:37 -0700

    add skeleton for networking test

--- src/test/java/NetworkingTest.java
+++ src/test/java/NetworkingTest.java
@@ -1,12 +1,51 @@
+import networkingutilities.ConnectionInterfacer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("networkingutilities test")
-public class NetworkingTest {
+@Nested
+@DisplayName("Server test")
+class ServerTest {
+
+	private static ConnectionInterfacer server;
+
+	@BeforeEach
+	void setupServer() {
+		String[] args = {"40" };
+		server = new ConnectionInterfacer(args);
+	}
 
 	@Test
 	void connect() {
+		server.launchConnection();
+	}
 
+	@Test
+	void invalidConnection() {
+
+	}
+
+}
+
+@Nested
+@DisplayName("Client test")
+class ClientTest {
+
+	private static ConnectionInterfacer server;
+	private static ConnectionInterfacer client;
+
+	@BeforeEach
+	void setupServerAndClient() {
+		String[] serverArgs = {"40" };
+		server = new ConnectionInterfacer(serverArgs);
+		String[] clientArgs = {"local", "50" };
+		client = new ConnectionInterfacer(clientArgs);
+	}
+
+	@Test
+	void connect() {
+		server.launchConnection();
 	}
 
 	@Test
